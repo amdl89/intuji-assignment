3cf164790ba556253ed1a9abd017dafcad5f7f33
Amul Dahal <amuldahal89@gmail.com>
2024-07-13 21:02:50 +0545

    add endpoint for request id token and save userinfo in db if not already present; login user

--- public/oauth/callback/login.php
+++ public/oauth/callback/login.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__.'/../../../bootstrap/app.php';
 
+use App\Auth\AuthFactory;
+use App\Database\DBFactory;
+use App\Utils\Config;
 use App\Utils\Redirect;
 use Google\Service\Oauth2;
 
@@ -12,35 +15,57 @@ if (!isset($_GET['code'])) {
     Redirect::path('/welcome.php');
 }
 
+$googleConfig = Config::get('google');
+
 $client = new Google_Client();
-$client->setClientId($config['google_client_id']);
-$client->setClientSecret($config['google_client_secret']);
-$client->setRedirectUri($config['google_redirect_uri']);
+$client->setClientId($googleConfig['google_client_id']);
+$client->setClientSecret($googleConfig['google_client_secret']);
+$client->setRedirectUri($googleConfig['google_login_redirect_uri']);
 
 try {
-    $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $accessToken = $client->fetchAccessTokenWithAuthCode($_GET['code'], $_SESSION['code_verifier']);
+    unset($_SESSION['code_verifier']);
 
-    // Check for errors in the access token response
     if (isset($accessToken['error'])) {
-        throw new \Exception('Error fetching access token: '.$accessToken['error']);
-    }
-
-    if (session_status() === PHP_SESSION_ACTIVE) {
-        session_unset();
-        session_destroy();
-        session_start();
+        throw new \Exception('Error fetching token for user: '.$accessToken['error']);
     }
 
     $oauth2 = new Oauth2($client);
     $userInfo = $oauth2->userinfo->get();
 
-    $_SESSION['user_info'] = $userInfo;
-    $_SESSION['access_token'] = $accessToken;
+    $db = DBFactory::getDB();
+    $auth = AuthFactory::getAuth();
+
+    $userQuery = 'SELECT id, sid FROM users where sid = :sid LIMIT 1';
+    if($user = $db->fetchOne($userQuery, [':sid' => $userInfo->getId()])) {
+        // user already exists, login user and redirect to home
+        if($auth->loginUsingId($user['id'])) {
+            Redirect::path('/home.php');
+        }
+        throw new \Exception('Login failed');
+    }
+
+    // register user, login user and redirect to home
+    $insertUserQuery = 'INSERT INTO USERS (sid, email, name, picture) VALUES (:sid, :email, :name, :picture)';
+    $insertUserQueryParams = [
+        ':sid' => $userInfo->getId(),
+        ':email' => $userInfo->getEmail(),
+        ':name' => $userInfo->getName(),
+        ':picture' => $userInfo->getPicture(),
+    ];
+    if($db->query($insertUserQuery, $insertUserQueryParams)) {
+        if($auth->loginUsingId($db->getLastInsertId())) {
+            Redirect::path('/home.php');
+        }
+        throw new \Exception('Login failed');
+    }
+    throw new \Exception('Registration failed');
 
 } catch (\Exception $e) {
-    $_SESSION['FLASH_MESSAGE'] = [
+    $errMsg = $e->getMessage() ?? 'Something went wrong';
+    $_SESSION['__FLASH_MESSAGE'] = [
         'type' => 'error',
-        'message' => 'Authorization error occurred. Please try again.',
+        'message' => "{$errMsg}. Please try again.",
     ];
     Redirect::path('/welcome.php');
 }
