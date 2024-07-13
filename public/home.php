85d77e6d004c6905f09dae4bc1f2e5572cbfa5f1
Amul Dahal <amuldahal89@gmail.com>
2024-07-13 20:59:21 +0545

    initial home page skeleton to show user not connected ui

--- public/home.php
+++ public/home.php
@@ -1,1 +1,22 @@
 <?php
+
+use App\Auth\AuthFactory;
+
+require_once __DIR__.'/../bootstrap/app.php';
+
+$auth = AuthFactory::getAuth();
+$authUserInfo = $auth->getUserInfo();
+
+$PAGE_TITLE = 'Home';
+
+if (!$authUserInfo['calendar_access_token']) {
+    // New user
+    $calendarNotConnected_newUser = true;
+    require __DIR__.'/../src/Views/calendar/userNotConnected.php';
+} elseif (!$authUserInfo['calendar_refresh_token']) {
+    // Refresh token has been revoked
+    $calendarNotConnected_newUser = false;
+    require __DIR__.'/../src/Views/calendar/userNotConnected.php';
+} else {
+    // show events view
+}
