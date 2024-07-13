b718b7dabf3e77448885ee01c899a39c84b6f250
Amul Dahal <amuldahal89@gmail.com>
2024-07-13 21:13:44 +0545

    complete home page views

---
+++ src/views/calendar/eventsListFetchError.php
@@ -0,0 +1,10 @@
+<?php require __DIR__ . '/../partials/header.php' ?>
+    <main class="container d-flex justify-content-center align-items-center vh-100">
+        <div class="d-flex flex-column align-items-center p-3 border-3">
+            <h1 class="mb-3">
+                Error fetching events.
+            </h1>
+            <h5 class="mb-1">Refresh page to try again</h5>
+        </div>
+    </main>
+<?php require __DIR__ . '/../partials/footer.php' ?>
