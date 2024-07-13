3c883bb4e1944a6e0db477578cdcfd32f6a5ee09
Amul Dahal <amuldahal89@gmail.com>
2024-07-13 21:01:50 +0545

    initial openid login request to request user info

--- public/oauth/login.php
+++ public/oauth/login.php
@@ -5,6 +5,10 @@ use App\Utils\Config;
 use App\Utils\Redirect;
 use Google\Service\Oauth2;
 
+$codeVerifier = bin2hex(random_bytes(32)); // Generate a random code verifier
+$codeChallenge =  rtrim(strtr(base64_encode(hash('sha256', $codeVerifier, true)), '+/', '-_'), '=');
+$_SESSION['code_verifier'] = $codeVerifier;
+
 $googleConfig = Config::get('google');
 
 $client = new Google_Client();
@@ -16,8 +20,13 @@ $client->setScopes([
     Oauth2::USERINFO_EMAIL,
     Oauth2::USERINFO_PROFILE,
 ]);
+$client->setPrompt('select_account');
+$client->setApprovalPrompt('force');
 $client->setAccessType('online');
 
-$authUrl = $client->createAuthUrl();
+$authUrl = $client->createAuthUrl(queryParams: [
+    'code_challenge' => $codeChallenge,
+    'code_challenge_method' => 'S256',
+]);
 
 Redirect::path($authUrl);
