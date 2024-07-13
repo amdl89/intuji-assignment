36d78a57cbc0ff202e9a1303aa682b2f5f20c3dc
Amul Dahal <amuldahal89@gmail.com>
2024-07-13 21:21:47 +0545

    compete endpoint for logout

--- public/logout.php
+++ public/logout.php
@@ -1,1 +1,11 @@
 <?php
+require_once __DIR__.'/../bootstrap/app.php';
+
+use App\Utils\Redirect;
+
+if (session_status() === PHP_SESSION_ACTIVE) {
+    session_unset();
+    session_destroy();
+}
+
+Redirect::path('/welcome.php');
